db698824be95b781206b5bbf655e381ce5eea852
henryhrlim <33216909+henryhrlim@users.noreply.github.com>
2018-04-13 16:06:19 +0800

    added location markers and working on portable charger borrowing

--- app/src/main/java/com/se2006/teamkaydon/powerlah/PortableChargerActivity.java
+++ app/src/main/java/com/se2006/teamkaydon/powerlah/PortableChargerActivity.java
@@ -1,14 +1,26 @@
 package com.se2006.teamkaydon.powerlah;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class PortableChargerManager {
+public class PortableChargerActivity extends AppCompatActivity {
     private int stationChargerAmt = 20;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portable_charger);
+
+
+
+
+    }
     public void BorrowPortable(String stationIndex){
         DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
         final DatabaseReference mStationRef = mRootRef.child("chargingstation").child(stationIndex);
